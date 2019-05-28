ccdfad346b8936943372d60ed171dd14ae006fcf
Allex Rodrigues <allexxrodriguess@gmail.com>
2019-05-28 01:06:54 -0300

    Preparação ambiente

---
+++ src/views/Theme.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="col-md-12">
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+  </div>
+</template>
