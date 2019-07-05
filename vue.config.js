def01bc522b44e54489f739abb0a0eb821781903
Allex Rodrigues <allexxrodriguess@gmail.com>
2019-07-05 20:38:26 -0300

    Configurado ambiente para deploy no GitHub Pages e Heroku

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,22 @@ module.exports = {
     devServer: {
         host:'tcc.site'
     },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/TCC-IFSP-SITE'
-    : '/'
+    publicPath: getPublicPath () 
+}
+
+function getPublicPath () {
+    let publicPath = '/';
+
+    if(process.env.NODE_ENV === 'production') {
+        if (process.env.HOST == 'github') {
+            console.log('Building for GitHub Pages...');
+            publicPath = '/TCC-IFSP-SITE';
+        }
+        else {
+            console.log('Building Heroku app...');
+        }
+    }
+
+    console.log('webpack publicPath: ', publicPath);
+    return publicPath;
 }
