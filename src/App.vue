de48417f596ffa2abbd10d3033ed8444b3e23343
Allex Rodrigues <allexxrodriguess@gmail.com>
2019-07-05 21:22:51 -0300

    Ajuste para correção de deploy no heroku

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,16 @@
 </template>
 
 <style src="./scss/main.scss" lang="scss"></style>
+
+<script>
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+export default {
+  name: 'app',
+  mounted() {
+    const env = runtimeEnv();
+    console.log(env.JS_RUNTIME_MESSAGE || 'JS_RUNTIME_MESSAGE is empty.');
+  }
+}
+</script>
+
