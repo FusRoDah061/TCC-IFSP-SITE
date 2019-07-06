9c1e25aa783a5f3de67e73de782bfc8344b884a7
Allex Rodrigues <allexxrodriguess@gmail.com>
2019-07-05 22:10:51 -0300

    COnfigurado express para servir apenas index.html

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-
-app = express();
-
-app.use(express.static('dist'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
-
-var port = process.env.PORT || 5000;
+const express = require('express');
+const serveStatic = require("serve-static");
+const path = require('path');
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
